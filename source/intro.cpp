de151ea1cd5b9bf677e8f0745aa329ad4098bddd
Knucklesfan <bigfootreserchersoftexas@gmail.com>
2023-09-16 12:55:54 -0500

    updated renderer, continued title screen

--- source/intro.cpp
+++ source/intro.cpp
@@ -1,32 +1,73 @@
 #include "intro.h"
 #include <nds.h>
 #include <stdio.h>
-#include "thugbob.h"
+#include "lakepng.h"
+#include "surprisingly.h"
 PrintConsole Intro::bottomScreen;
 PrintConsole Intro::topScreen;
-int Intro::bg = 0;
+int Intro::bg[2];
 int Intro::angle = 0;
+int Intro::knuxfanx = 0;
 void Intro::load() {
     videoSetModeSub(MODE_0_2D);
-    videoSetMode(MODE_5_2D);
-    vramSetBankA(VRAM_A_MAIN_BG);
-	vramSetBankB(VRAM_B_MAIN_BG);
-	vramSetBankC(VRAM_C_SUB_BG);
-	bg = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+    videoSetMode(MODE_0_2D);
+	//using mode 0 for both for MAXIMUM BACKGROUNDS!!!
+
+	vramSetBankA(VRAM_A_MAIN_BG);
+  	vramSetBankC(VRAM_C_SUB_BG);
+
+  	bgExtPaletteEnable();
+
+	//initialize the memory for our two backgrounds
+	bg[0] = bgInit   (0, BgType_Text8bpp, BgSize_T_256x256, 31, 0);
+	bg[1] = bgInit   (1, BgType_Text8bpp, BgSize_T_256x256, 30, 4);
+
+	dmaCopy(lakepngTiles,  bgGetGfxPtr(bg[1]), lakepngTilesLen);
+	dmaCopy(surprisinglyTiles, bgGetGfxPtr(bg[0]), surprisinglyTilesLen);
+
+	dmaCopy(lakepngMap,  bgGetMapPtr(bg[1]), lakepngMapLen);
+	dmaCopy(surprisinglyMap, bgGetMapPtr(bg[0]), surprisinglyMapLen);
+
+ 	vramSetBankE(VRAM_E_LCD); // for main engine
+	dmaCopy(lakepngPal,  &VRAM_E_EXT_PALETTE[1][0],  lakepngPalLen);  // bg 1, slot 0
+    dmaCopy(surprisinglyPal,  &VRAM_E_EXT_PALETTE[0][0],  surprisinglyPalLen);  // bg 0, slot 0
+ 	vramSetBankE(VRAM_E_BG_EXT_PALETTE);     // for main engine
+
 	consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
-	consoleInit(&topScreen, 0, BgType_Text4bpp, BgSize_T_256x256, 9606+128, 9606, true, true);
+	
+	// consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 30, 31, true, true);
 
-	decompress(thugbobBitmap, BG_GFX, LZ77Vram);
+    // consoleSelect(&topScreen);
+	// iprintf("\x1b[2J");
+	// iprintf("\x1b[0m");
 
-    consoleSelect(&topScreen);
-    iprintf("DSDonut!");
+    // iprintf("DSDonut!");
 	consoleSelect(&bottomScreen);
 	iprintf("Press start...");
 	
 }
 int Intro::logic() {
-	bgSetCenter(bg, 0, 0);
-	bgSetRotate(bg, angle);
+	// bgSetScroll(bg[1], 0, 0);
+	bgSetScroll(bg[0], angle, angle);
+
 	bgUpdate();
+	    scanKeys();
+    u16 keys = keysDown();
+    if (keys & KEY_L)
+    {
+        if (knuxfanx > -16)
+        {
+            knuxfanx--;
+        }
+    };
+    if (keys & KEY_R)
+    {
+        if (knuxfanx < 0)
+        {
+            knuxfanx++;
+        }
+    }
+	setBrightness(1,knuxfanx);
+	angle++;
 	return 0;
 }
--- source/intro.h
+++ source/intro.h
@@ -4,7 +4,15 @@ namespace Intro {
     extern PrintConsole topScreen;
     extern PrintConsole bottomScreen;
     extern bool active;
-    extern int bg;
+    extern int bg[2];
+    extern int knuxfan[2];
+    extern int presents[2];
+    extern int knuxfany;
+    extern int knuxfanx;
+    extern int presentsy;
+    extern int presentsx;
+    extern int knuxfanangle;
+    extern int presentsangle;
     extern int angle;
     extern bool godown;
     void load();
